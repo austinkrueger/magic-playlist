1a5fdf4e27db8abaf86d3eb325a96891a8c88301
Austin <akruegr@gmail.com>
2020-06-16 18:25:51 -0500

    start getting into the nitty gritty

---
+++ mp-back/routes/spotify.js
@@ -0,0 +1,21 @@
+let SpotifyWebApi = require('spotify-web-api-node');
+
+/*
+    GET request with data from Spotify Login
+*/
+function loginToSpotify() {}
+
+// post to api
+function createPlaylist() {}
+
+// post to api
+function searchArtist() {}
+
+// post to api
+function getArtist() {}
+
+// post to api
+function getArtistRelatedArtists() {}
+
+// post to api
+function getArtistTopSongs() {}
