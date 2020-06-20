1e5cfeff2cff29c3797a39678f7159e50453947c
Austin <akruegr@gmail.com>
2020-06-20 17:57:20 -0500

    send temp playlist data to secondary viewing component

--- mp-front/src/app/app-routing.module.ts
+++ mp-front/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'playlist', component: PlaylistComponent },
   { path: 'auth/spotify_login', component: AuthComponent },
   { path: 'me', component: DashboardComponent },
 ];
--- mp-front/src/app/components/playlist/playlist.component.ts
+++ mp-front/src/app/components/playlist/playlist.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.scss']
+  styleUrls: ['./playlist.component.scss'],
 })
 export class PlaylistComponent implements OnInit {
-
-  constructor() { }
+  playlist: any;
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(
+      JSON.parse(sessionStorage.getItem('userGeneratedTempPlaylist'))
+    );
+    this.playlist = JSON.parse(
+      sessionStorage.getItem('userGeneratedTempPlaylist')
+    );
   }
 
+  loginSpotify() {}
 }
