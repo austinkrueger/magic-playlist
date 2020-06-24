d7a9024afc3978fe68c12eab167d8cd032296431
Austin <akruegr@gmail.com>
2020-06-23 21:17:42 -0500

    update service methods

--- mp-front/src/app/services/playlist.service.ts
+++ mp-front/src/app/services/playlist.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaylistService {
+  uri = 'http://localhost:4000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPlaylistsByUser(userId: string): Observable<any> {
+    return this.http.get(`${this.uri}/playlist`, {
+      params: { user_id: userId },
+    });
+  }
+
+  getPlaylist(playlistId: string): Observable<any> {
+    return this.http.get(`${this.uri}/playlist/${playlistId}`);
+  }
+
+  addPlaylist(playlist: any): Observable<any> {
+    const headers = new HttpHeaders();
+    const token = '';
+    headers.append('Authorization', `Bearer ${token}`);
+    return this.http.post(
+      `${this.uri}/playlist`,
+      {
+        playlist: playlist,
+      },
+      {
+        headers: headers,
+      }
+    );
+  }
+
+  updatePlaylist(playlist: any): Observable<any> {
+    const headers = new HttpHeaders();
+    const token = '';
+    headers.append('Authorization', `Bearer ${token}`);
+    return this.http.put(
+      `${this.uri}/playlist/${playlist.id}`,
+      {
+        playlist: playlist,
+      },
+      {
+        headers: headers,
+      }
+    );
+  }
+
+  deletePlaylist(playlistId: string): Observable<any> {
+    const headers = new HttpHeaders();
+    const token = '';
+    headers.append('Authorization', `Bearer ${token}`);
+    return this.http.delete(`${this.uri}/playlist/${playlistId}`, {
+      headers: headers,
+    });
+  }
 }
