d8775616d922ffa2c7fa45e9753c0ff726db4b18
Austin <akruegr@gmail.com>
2020-06-26 20:34:58 -0500

    update test for authinterceptor

--- mp-front/src/app/interceptors/auth.interceptor.spec.ts
+++ mp-front/src/app/interceptors/auth.interceptor.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { PlaylistService } from '../services/playlist.service';
 
 import { AuthInterceptor } from './auth.interceptor';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  let httpMock: HttpTestingController;
+  let service: PlaylistService;
+  const userId = '1275938982';
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+      ],
+    })
+  );
+  service = TestBed.inject(PlaylistService);
+  httpMock = TestBed.inject(HttpTestingController);
 
   it('should be created', () => {
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should add an Authorization header', () => {
+    service.getPlaylistsByUser(userId).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(`${service.uri}/playlist`);
+
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
   });
 });
