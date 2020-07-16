68c6c04cd1af77a7694eef140f2b4ff3071784cc
Austin <akruegr@gmail.com>
2020-07-16 17:37:49 -0500

    remove default api response

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-app.get('/', (req, res) => res.json({ message: 'magic-playlist api' }));
+// app.get('/', (req, res) => res.json({ message: 'magic-playlist api' }));
 
 app
   .route('/playlist')
